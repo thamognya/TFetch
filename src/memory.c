fc37c751824cd8f8496300d93969b5a590f692e2
Thamognya Kodi <contact@thamognya.com>
2022-06-06 08:22:11 +0700

    update

--- src/memory.c
+++ src/memory.c
@@ -5,13 +5,27 @@
 #elif __APPLE__
 #endif
 
+// colors
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
 int memory()
 {
     #ifdef __linux__
         // make type info
         struct sysinfo info;
         sysinfo(&info);
-        printf("total ram: %s", info.totalram);
+        printf(RED "%s" RESET, "RAM: ");
+        printf("%02ld", (((info.freeram)/1024)/1024));
+        printf(BLU "%s" RESET, "/");
+        printf("%02ld", (((info.totalram)/1024)/1024));
+        printf("\n");
         return 0;
     #elif __APPLE__
     return 0;
