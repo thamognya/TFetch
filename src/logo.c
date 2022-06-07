af8228a22bf91be09ae92cf16608ce55226d19e1
Thamognya Kodi <contact@thamognya.com>
2022-06-07 19:46:35 +0700

    logo

--- src/logo.c
+++ src/logo.c
@@ -9,10 +9,34 @@
 int logo()
 {
     #ifdef __linux__
-    	printf("%s\n", "tmp");
+    /*
+        for testing purposes
+    	printf("%s\n", "  ____            _              ");
+    	printf("%s\n", " / ___| ___ _ __ | |_ ___   ___  ");
+    	printf("%s\n", "| |  _ / _ \\ '_ \\| __/ _ \\ / _ \\ ");
+    	printf("%s\n", "| |_| |  __/ | | | || (_) | (_) |");
+    	printf("%s\n", " \\____|\\___|_| |_|\\__\\___/ \\___/ ");
+    */
+    /*  contributing guideline
+        firstly use figlet
+        secondly paste the figlet in a multiline comments before using the printf 
+        if there are \ replace them with \\ in the printf (leaving the comment intact)
+        use the macos one as an example
+    */
+        return 0;
     #elif __APPLE__
-        printf("%s\n", "");
+        /*
+                               ___  ____
+         _ __ ___   __ _  ___ / _ \/ ___|
+        | '_ ` _ \ / _` |/ __| | | \___ \
+        | | | | | | (_| | (__| |_| |___) |
+        |_| |_| |_|\__,_|\___|\___/|____/
+        */
+        printf("%s\n", "                       ___  ____");
+        printf("%s\n", " _ __ ___   __ _  ___ / _ \\/ ___|");
+        printf("%s\n", "| '_ ` _ \\ / _` |/ __| | | \\___ \\");
+        printf("%s\n", "| | | | | | (_| | (__| |_| |___) |");
+        printf("%s\n", "|_| |_| |_|\\__,_|\\___|\\___/|____/");
+        return 0;
     #endif
-
-    return 0;
 }
--- tmp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-tmp=$(echo "5.17.0-gentoo-x86_64" | grep -Eo "^.*[a-zA-Z]+.*$")
-echo $tmp
