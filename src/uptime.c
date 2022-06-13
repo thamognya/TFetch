f512ae19dd40d97a9a65f9ab768aa6658c9b4ac3
betty2310 <huynh.dh2310@gmail.com>
2022-06-13 17:41:27 +0700

    feat(uptime): update for OSX

--- src/uptime.c
+++ src/uptime.c
@@ -6,6 +6,7 @@
     #include <sys/sysinfo.h>
 #elif __APPLE__
     #include <sys/sysctl.h>
+    #include <time.h>
 #endif
 // get colors
 #include "./include/color.h"
@@ -33,7 +34,25 @@ void uptime()
         // print new line
         printf("\n");
     #elif __APPLE__
-        // need to find way to get uptime future
+        struct timeval boottime;
+        time_t bsec = boottime.tv_sec, csec = time(NULL);
+        double uptime = difftime(csec, bsec);
+        // print uptime
+        printf(RED "%s" RESET, "UPTIME: ");
+        // print the uptime / 3600 to get hours
+        printf("%02ld ", (int) uptime / 3600);
+        // print hours
+        printf(BLU "%s " RESET, "hours");
+        // print the uptime in hours
+        printf("%02ld ", (int)uptime % 3600 / 60);
+        // print mins
+        printf(BLU "%s " RESET, "mins");
+        // print hte up time in seconds
+        printf("%02ld ", (int)uptime % 60);
+        // print seconds
+        printf(BLU "%s " RESET, "secs");
+        // print new line
+        printf("\n");
     #else
         printf("%s\n", "Your OS is not supported. Please make a pr at https://github.com/Thamognya/TFetch");
     #endif
